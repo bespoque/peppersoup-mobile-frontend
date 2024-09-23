3aaa96de4b458d5f71f45df5e01e27a15aab910f
prinxard <prinxard@gmail.com>
2024-09-23 04:30:55 +0100

    update

--- src/app/(dashboard)/menus/page.tsx
+++ src/app/(dashboard)/menus/page.tsx
@@ -1,10 +1,52 @@
-
+"use client";
 import MenusFoodMenu from "@/src/components/MenusFoodMenuTabs";
-import React from "react";
+import React, { useRef, useState } from "react";
 
 export default function Menus() {
+  const [showPopup, setShowPopup] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
+  const togglePopup = () => {
+    setShowPopup((prev) => !prev);
+  };
   return (
     <div>
+      <div className="flex justify-end items-center m-4">
+        <button
+          ref={buttonRef}
+          onClick={togglePopup}
+          className="bg-paleGreen text-black py-2 px-4 rounded shadow"
+        >
+          Add New Menu Item
+        </button>
+      </div>
+      {showPopup && buttonRef.current && (
+        <div
+          className="mt-1 p-4 bg-white border rounded shadow absolute z-10"
+          style={{
+            top:
+              buttonRef.current.getBoundingClientRect().bottom + window.scrollY,
+            left: buttonRef.current.getBoundingClientRect().left,
+          }}
+        >
+          <ul className="space-y-2">
+            <li>
+              <button onClick={() => console.log("Add to Peppersoup Menu")}>
+                To Peppersoup Menu
+              </button>
+            </li>
+            <li>
+              <button onClick={() => console.log("Add to Sides Menu")}>
+                To Sides Menu
+              </button>
+            </li>
+            <li>
+              <button onClick={() => console.log("Add to Drinks Menu")}>
+                To Drinks Menu
+              </button>
+            </li>
+          </ul>
+        </div>
+      )}
       <MenusFoodMenu />
     </div>
   );
--- src/components/MenusFoodMenuTabs.tsx
+++ src/components/MenusFoodMenuTabs.tsx
@@ -7,60 +7,16 @@ import Tabs from "./Tabs";
 const MenusFoodMenu = () => {
   const tabLabels = ["on-the-menu", "all-saved-items"] as string[];
   const [activeDropdown, setActiveDropdown] = useState<number | null>(null);
-  const [showPopup, setShowPopup] = useState(false);
-  const buttonRef = useRef<HTMLButtonElement | null>(null);
 
   const handleDropdownToggle = (index: number) => {
     setActiveDropdown((prev) => (prev === index ? null : index));
   };
 
-  const togglePopup = () => {
-    setShowPopup((prev) => !prev);
-  };
-
   const tabContent = {
     "on-the-menu": (
       <>
-      <h5 className="font-bold">Menus Items for today</h5>
-        <div className="flex justify-between items-center">
-          <h5 >Peppersoup menu</h5>
-          <button
-            ref={buttonRef}
-            onClick={togglePopup}
-            className="bg-paleGreen text-black py-2 px-4 rounded shadow"
-          >
-            Add New Menu Item
-          </button>
-        </div>
-        {showPopup && buttonRef.current && (
-          <div
-            className="mt-1 p-4 bg-white border rounded shadow absolute z-10"
-            style={{
-              top:
-                buttonRef.current.getBoundingClientRect().bottom +
-                window.scrollY,
-              left: buttonRef.current.getBoundingClientRect().left,
-            }}
-          >
-            <ul className="space-y-2">
-              <li>
-                <button onClick={() => console.log("Add to Peppersoup Menu")}>
-                  To Peppersoup Menu
-                </button>
-              </li>
-              <li>
-                <button onClick={() => console.log("Add to Sides Menu")}>
-                  To Sides Menu
-                </button>
-              </li>
-              <li>
-                <button onClick={() => console.log("Add to Drinks Menu")}>
-                  To Drinks Menu
-                </button>
-              </li>
-            </ul>
-          </div>
-        )}
+        <h5 className="font-bold">Menus Items for today</h5>
+        <h4>Peppersoup</h4>
         <div className="overflow-x-auto mt-4">
           <div className="flex gap-6">
             {MenuData.PepperSoup.map((item, index) => (
